c81cd0d6a95468cbe1012a7a5794aa5ba8137626
Tia Curry <tiaanicolee@gmail.com>
2016-10-02 05:02:35 +0000

    added form tag

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    @game.guess(letter)
     redirect '/show'
   end
   
